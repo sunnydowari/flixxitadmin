924eaf863a22ce66744d4f0ced0745648dd9d5de
blueyottle <sunnydowari@gmail.com>
2023-07-19 01:04:48 +0530

    implemented context usage

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { AuthContextProvider } from "./context/authContext/AuthContext";
+import { MovieContextProvider } from "./context/movieContext/MovieContext";
+import { ListContextProvider } from "./context/listContext/ListContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AuthContextProvider>
+    <MovieContextProvider>
+      <ListContextProvider>
+        <App />
+      </ListContextProvider>
+    </MovieContextProvider>
+  </AuthContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
